bf7af7ff7052159d7e73e9bef9bcfd0992bcb755
Mahafizur Rahman <LnG2509P0074@clx.dc>
2026-05-20 18:05:41 +0600

    change the massage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import OrderSection from "@/components/sections/OrderSection";
 export default function Home() {
   return (
     <>
-      <OrderSection />
+      <h1>This link doesn&apos;t exist</h1>
+      <p>Please go to the presswayy intagration group for new link</p>
     </>
   );
 }
